7258fa3e66a200ab67036f0c609f4de26c9fc1f8
Matt Callaghan <mcallaghan@naperville203.org>
2025-10-23 14:17:42 -0500

    wrote ch 5 notes on string comparisons

--- Conditions.java
+++ Conditions.java
@@ -79,14 +79,83 @@ public class Conditions
             System.out.println("rolled a four");
         }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
     }
+    
+    
+    public static boolean doublesAreEqual(double num1, double num2)
+    {
+        /*
+         * If we use the equality operator with doubles, it will only return true if all binary digits match.
+         * 
+         * For "equal" numbers, this is probably not what we want to use due to floating point rounding.
+         * 
+         * We will check if they are "close enough"  (i.e., epsilon value)
+         */
+        
+        final double EPSILON = 1e-6;
+        
+        if(Math.abs(num1 - num2) < EPSILON)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public static void stringExample()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter two words: ");
+        String str1 = s.next();
+        String str2 = s.next();
+        //str2 = str1;  // create an alias
+        
+        /*
+         * The equality operator (==) returns true is the two variables (str and str2) contain the same value.
+         *      For variables of type String, or any class, this means they conatain the same reference to 
+         *      the same object in memory, NOT that the strings have the same sequence of characters.
+         *      
+         * Alias: two variables refer to the same object.
+         */
+        if(str1 == str2)
+        {
+            System.out.println("String references are equal");
+        }
+        else
+        {
+            System.out.println("String  references are NOT equal");
+        }
+        
+        /*
+         * The equals method returns true id the two objects referenced by the variables are "equal". For Strings,
+         *      it means the two objects have the same sequence of characters.
+         */
+        
+        if(str1.equals(str2))
+        {
+            System.out.println("the strings are equal");
+        }
+        else
+        {
+            System.out.println("the strings are NOT equal");
+        }
+        
+        /*
+         * If we want to check if the strings are not equal, we use logical complement
+         */
+        if(! str1.equals(str2))
+        {
+            System.out.println("the strings are NOT equal");
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
