0c16251b2c67b6476983318866a9a461f4746b80
Matt Callaghan <mcallaghan@naperville203.org>
2025-11-03 08:46:23 -0600

    Wrote Ch 6 note son while loops

--- Iterations.java
+++ Iterations.java
@@ -3,10 +3,51 @@ import java.util.Scanner;
 /**
  * Examples and notes on iterations (Chapter 6)
  *
- * @author 
- * @version 
+ * @author mrcallaghan
+ * @version 03nov2025
  */
 public class Iterations
 {
-    
+    public static void whileExample()
+    {
+        /*
+         * while loop:
+         * 
+         *      evaluates a condition (i.e., boolean expression)
+         *        if true, executes the body of the loop and then reevaluates the condition
+         *        if false, skips the body of the loop and continues
+         */
+        
+        int count = 1;              // initialization
+        
+        while (count <= 5)          // condition
+        {
+            System.out.println(count);      // loop body
+            
+            count++;                        // update loop variable
+        }
+        
+        System.out.println("done.");
+        
+        /*
+         * This is an example of an infinte loop!
+         * 
+         * A better condition would be:
+         *      while (count < 50):
+         * 
+         */
+        
+        count = 1;
+        
+        while(count != 50)
+        {
+            System.out.println(count);
+            
+            count += 2;
+        }
+        
+        System.out.println("done.");
+        
+        
+    }
 }
