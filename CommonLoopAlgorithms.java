849de3f029381ad00bcd4118f88502b2c9e80d98
Matt Callaghan <mcallaghan@naperville203.org>
2025-11-07 08:31:52 -0600

    Added common loop algorithms

--- CommonLoopAlgorithms.java
+++ CommonLoopAlgorithms.java
@@ -3,14 +3,30 @@ import java.util.Scanner;
 public class CommonLoopAlgorithms
 {
     /*
-     * Loop Algorithm #1: Count Matches for Length
+     * Loop Algorithm #1: Count Matches for Length (
      * reads a series of words (ends with "quit")
      * counts the number of words of length greater than four
      * @return the number of words of length greater than four
      */
     public static int countMatchesForLength()
     {
-        return 0;
+        Scanner s = new Scanner(System.in); //initializes scanner
+        int count = 0; //initializes count. Will keep track of words over 4 letters long
+        String word; //initializes word. Will store each word inputted
+
+        System.out.println("Enter your words (type quit to stop): "); //prompts user to enter words
+        word = s.next(); //reads first word in print statement
+
+        while(!word.equals("quit")) //repeats until user types the word "quit"
+        {
+            if (word.length() > 4) //checks if word is over 4 letters long
+            {
+                count++; //adds 1 to count if while statement is true
+            }
+            word = s.next(); //reads next word
+        }
+
+        return count; //returns total count
     }
 
     /*
@@ -21,39 +37,113 @@ public class CommonLoopAlgorithms
      */
     public static int countMatchesForPrefix()
     {
-        return 0;
+        Scanner s = new Scanner(System.in);
+
+        //Variables used in code
+        int counter = 0;
+        int spaceFinder = 0;
+        boolean quit = false;
+        String sentence = "";
+
+        while(!quit)
+        {
+            System.out.println("Type your sentence (Type 'quit' to quit): ");
+            sentence = s.nextLine();
+            sentence = sentence.toLowerCase();
+
+            if(sentence.equals("quit"))
+            {
+                return counter;
+            }
+
+            //Uses spaces to separate the words
+            String[] words = sentence.split(" ");
+
+            for (String word : words)
+            {
+                if(word.startsWith("con"))
+                {
+                    counter++;
+                }
+            }     
+        }
+        return counter;
     }
 
     /*
-     * Loop Algorithm #3: Count Matches for Suffix
+     * Loop Algorithm #3: Count Matches for Suffix  (Joshua)
      * reads a series of words (ends with "quit")
      * counts the number of words that end with the substring "est"
      * @return the number of words  that end with the substring "est"
      */
     public static int countMatchesForSuffix()
     {
-        return 0;
+        Scanner eii = new Scanner(System.in);
+        int count = 0;
+        System.out.println("enter words (quit to end): ");
+        while (true)
+        {
+            String w = eii.next();
+            if (w.equals("quit"))
+                break;
+            if (w.endsWith("est"))
+                count++;
+        }
+
+        return count;
     }
 
     /*
-     * Loop Algorithm #4: Reverse String
+     * Loop Algorithm #4: Reverse String (Abdul-rahman)
      * reads one word
      * reverses the characters in the word
      * @return a new string with the characters reversed
      */
-    public static String reverseWord()
-    {
-        return "";
+    public static String reverseWord(String word)
+    {   String reversedWord = "";
+        // Apple
+        for (int i = word.length(); i >= 1; i --) {
+            reversedWord += word.substring(i-1, i);
+        }
+        return reversedWord;
     }
 
     /*
-     * Loop Algorithm #5: Compare Adjacent Words
+     * Loop Algorithm #5: Compare Adjacent Words 
      * reads a series of words until an adjacent duplicate word is entered
      * @return the number of unique words entered before the duplicate
      */
     public static int compareAdjacent()
     {
-        return 0;
+        Scanner s = new Scanner(System.in);
+        String[] inps = new String[1000];
+        int cnt = 0;
+        boolean unique = true;
+        String inp = "", lastInp = "";
+        do{
+            lastInp = inp;
+            cnt++;
+            inp = s.next();
+            unique = true;
+            for(int i = 0; i < cnt; i++)
+            {
+                if(inp.equals(inps[i]))
+                {
+                    unique = false;
+                    break;
+                }
+            }
+            // System.out.println(unique);
+            if(unique)
+            {
+                inps[cnt] = inp;
+            }
+            else
+            {
+                cnt--;
+            }
+        } while(!inp.equals(lastInp));
+        return cnt;
     }
 
     /*
@@ -65,7 +155,12 @@ public class CommonLoopAlgorithms
      */
     public static String promptUntilMatch()
     {
-        return "";
+        Scanner s = new Scanner(System.in);
+        String inp = "", lastInp = "";
+        do{
+            inp = s.next();
+        } while(!(inp.compareTo("north") > 0));
+        return inp;
     }
 
     /*
